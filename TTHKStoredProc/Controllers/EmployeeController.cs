8446b76c1468777b0baf4ef66caa56273a15393c
StefanRammo <stefanrammo85@gmail.com>
2024-05-21 15:59:44 +0300

    Employee Controller

---
+++ TTHKStoredProc/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TTHKStoredProc.Data;
+using TTHKStoredProc.Models;
+
+namespace TTHKStoredProc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public StoredProcDbContext _context;
+
+        public EmployeeController(StoredProcDbContext context) {_context = context;}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IEnumerable<Employee> SearchResult()
+        {
+            var result = _context.Employees
+                .FromSqlRaw<Employee>("spSearchEmployees")
+                .ToList();
+
+            return result;
+        }
+    }
+}
