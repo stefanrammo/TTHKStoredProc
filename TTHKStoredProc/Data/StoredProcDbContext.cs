cdd19e7e60bf0f91536bfc45484dd7c73b3dfb51
StefanRammo <stefanrammo85@gmail.com>
2024-05-21 15:08:00 +0300

    DbContext

--- TTHKStoredProc/Data/StoredProcDbContext.cs
+++ TTHKStoredProc/Data/StoredProcDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TTHKStoredProc.Models;
 
 namespace TTHKStoredProc.Data
 {
     public class StoredProcDbContext : DbContext
     {
-
+        public StoredProcDbContext(DbContextOptions<StoredProcDbContext> options)
+            : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
     }
 }
